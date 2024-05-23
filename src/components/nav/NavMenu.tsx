1cb4e1ba6c6d92d2fdcc337914e95b9912923bd6
ciobiano <rafaelobiano@yahoo.com>
2024-05-23 21:10:55 +0100

    chore: update package dependencies and formatting in code files

---
+++ src/components/nav/NavMenu.tsx
@@ -0,0 +1,133 @@
+"use client";
+
+import * as React from "react";
+import Link from "next/link";
+
+import { cn } from "@/lib/utils";
+import {
+	NavigationMenu,
+	NavigationMenuContent,
+	NavigationMenuItem,
+	NavigationMenuLink,
+	NavigationMenuList,
+	NavigationMenuTrigger,
+	navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import Icon from "../Icon";
+
+const components: { title: string; href: string; description: string }[] = [
+	{
+		title: "Alert Dialog",
+		href: "/docs/primitives/alert-dialog",
+		description:
+			"A modal dialog that interrupts the user with important content and expects a response.",
+	},
+	{
+		title: "Hover Card",
+		href: "/docs/primitives/hover-card",
+		description:
+			"For sighted users to preview content available behind a link.",
+	},
+	{
+		title: "Progress",
+		href: "/docs/primitives/progress",
+		description:
+			"Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+	},
+	{
+		title: "Scroll-area",
+		href: "/docs/primitives/scroll-area",
+		description: "Visually or semantically separates content.",
+	},
+	
+];
+
+export function NavMenu() {
+	return (
+		<NavigationMenu>
+			<NavigationMenuList>
+				<NavigationMenuItem>
+					<NavigationMenuTrigger>solutions</NavigationMenuTrigger>
+					<NavigationMenuContent>
+						<ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr] backdrop-blur-2xl ">
+							<li className="row-span-3">
+								<NavigationMenuLink asChild>
+									<a
+										className="flex h-full w-full select-none flex-col justify-end rounded-md b   p-6 no-underline outline-none focus:shadow-md  "
+										href="/"
+									>
+										<Icon name="home" className="h-6 w-6" />
+										<div className="mb-2 mt-4 text-lg font-medium">
+											Auto Refuel
+										</div>
+										<p className="text-sm leading-tight text-muted-foreground">
+											Beautifully designed components that you can copy and
+											paste into your apps. Accessible. Customizable. Open
+											Source.
+										</p>
+									</a>
+								</NavigationMenuLink>
+							</li>
+							<ListItem href="/docs" title="Introduction">
+								Re-usable components built using Radix UI and Tailwind CSS.
+							</ListItem>
+							<ListItem href="/docs/installation" title="Installation">
+								How to install dependencies and structure your app.
+							</ListItem>
+							
+						</ul>
+					</NavigationMenuContent>
+				</NavigationMenuItem>
+				<NavigationMenuItem>
+					<NavigationMenuTrigger>Components</NavigationMenuTrigger>
+					<NavigationMenuContent>
+						<ul className="grid w-[400px] gap-3 p-4 md:w-[500px] backdrop-blur-2xl md:grid-cols-2 lg:w-[600px] ">
+							{components.map((component) => (
+								<ListItem
+									key={component.title}
+									title={component.title}
+									href={component.href}
+								>
+									{component.description}
+								</ListItem>
+							))}
+						</ul>
+					</NavigationMenuContent>
+				</NavigationMenuItem>
+				<NavigationMenuItem>
+					<Link href="/docs" legacyBehavior passHref>
+						<NavigationMenuLink className={navigationMenuTriggerStyle()}>
+							Documentation
+						</NavigationMenuLink>
+					</Link>
+				</NavigationMenuItem>
+			</NavigationMenuList>
+		</NavigationMenu>
+	);
+}
+
+const ListItem = React.forwardRef<
+	React.ElementRef<"a">,
+	React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+	return (
+		<li>
+			<NavigationMenuLink asChild>
+				<a
+					ref={ref}
+					className={cn(
+						"block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+						className
+					)}
+					{...props}
+				>
+					<div className="text-sm font-medium leading-none">{title}</div>
+					<p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+						{children}
+					</p>
+				</a>
+			</NavigationMenuLink>
+		</li>
+	);
+});
+ListItem.displayName = "ListItem";
