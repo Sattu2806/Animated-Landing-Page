5e17ae979346840f742159510c33a33791c692ec
ciobiano <rafaelobiano@yahoo.com>
2024-06-06 19:34:08 +0100

    feat: add authentication middleware for protecting routes

---
+++ middleware.ts
@@ -0,0 +1,13 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+
+export default clerkMiddleware((auth, request) => {
+	if (!isPublicRoute(request)) {
+		auth().protect();
+	}
+});
+
+export const config = {
+	matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
