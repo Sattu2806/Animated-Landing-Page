72068bf489a8b82e7c1c0145d10874e2d8a264c5
ciobiano <rafaelobiano@yahoo.com>
2024-06-06 19:34:40 +0100

    chore: Add remotePatterns for lazy loading images

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ["pbs.twimg.com"],
+		remotePatterns: [
+			{
+				hostname: "pbs.twimg.com",
+			},
+		],
 	},
 };
 
